2438e618fdb938fbc65509fe54e53825f39f08b6
RafaelHuerta1 <010530713@alumno.utel.edu.mx>
2024-07-06 14:51:08 -0600

    chore: Refactor CrearOrc component and add createOracion function

--- app/(tabs)/CrearOrc.jsx
+++ app/(tabs)/CrearOrc.jsx
@@ -1,228 +1,206 @@
-import React, {useState , useEffect} from "react";
-import { View, Text, Button, TouchableOpacity, TextInput, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  Modal,
+} from "react-native";
 //import { creatOrc } from "../logica/index.js";
-import { Modal } from "react-native-web";
+
 
 const CrearOrc = () => {
+  const [nombre, setNombre] = useState("");
+  const [intencion, setIntencion] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+  const [txtModal, setTxtModal] = useState("");
 
+  const limpiarOracion = () => {
+    setIntencion("");
+    setNombre("");
+  };
 
-    const [nombre, setNombre] = useState('');
-    const [intencion, setIntencion] = useState('');
-    const [modalVisible, setModalVisible] = useState(false);
-    const [txtModal, setTxtModal] = useState('');
-
-
-    const limpiarOracion = () => {
-        setIntencion('');
-        setNombre('');
+  const creatOrc = () => {
+    if (nombre === "" || intencion === "") {
+      setModalVisible(true);
+      setTxtModal("Por favor ingresa un nombre e intención");
     }
+    else{
+      setModalVisible(true);
+      setTxtModal("Estamos creando tu oración ;)"); 
+      
+      // hacer la llamada a la api de gpt-3
+       
+    }
+  };
 
+  const cerrarModal = () => {
+    console.log("cerrar modal");
+    setModalVisible(false);
+  };
 
+  return (
+    <View>
+      <View
+        style={{
+          justifyContent: "flex-end",
+          alignItems: "flex-end",
+          marginTop: 15,
+          marginRight: 25,
+        }}
+      >
+        <Button
+          title="Iniciar Sesión"
+          color="#841584"
+          onPress={() => alert("Iniciar Sesión")}
+        />
+      </View>
 
-   const creatOrc =   () => {
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 40,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "500",
+            textAlign: "center",
+            margin: 20,
+          }}
+        >
+          Ora de manera personalizada para tus seres queridos y comparte en tus
+          redes sociales.
+        </Text>
+      </View>
 
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 80,
+        }}
+      >
+        <TextInput
+          onChangeText={(nombre) => setNombre(nombre)}
+          value={nombre}
+          style={{
+            height: "auto",
+            width: 300,
+            minHeight: 40,
+            borderColor: "gray",
+            borderWidth: 1,
+            borderRadius: 10,
+            textAlign: "center",
+          }}
+          placeholder="Nombre"
+        />
 
-        if (nombre === '' || intencion === '') {
-          
-            setModalVisible(true);
-            setTxtModal('Por favor ingresa un nombre e intención');
-        }
-        
-   }
+        <TextInput
+          onChangeText={(intencion) => setIntencion(intencion)}
+          value={intencion}
+          style={{
+            height: "auto",
+            width: 300,
+            minHeight: 40,
+            borderColor: "gray",
+            borderWidth: 1,
+            borderRadius: 10,
+            textAlign: "center",
+            marginTop: 10,
+          }}
+          placeholder="Intención"
+        />
 
-
-
-    return (
-        <View>
-            <View
+        <TouchableOpacity
+          onPress={creatOrc}
+          style={{
+            backgroundColor: "#841584",
+            width: 300,
+            height: 40,
+            borderRadius: 10,
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 20,
+          }}
+        >
+          <Text
             style={{
-                justifyContent: 'flex-end',
-                alignItems: 'flex-end',
-                marginTop:15,
-                marginRight: 25,
+              color: "white",
+              fontWeight: "bold",
             }}
-            
-            >
-                    <Button
-                    title="Iniciar Sesión"
-                    color="#841584"
-                    onPress={() => alert('Iniciar Sesión')}
+          >
+            Crear Oración
+          </Text>
+        </TouchableOpacity>
 
-                    
-                    /> 
-            </View>
-
+        <TouchableOpacity
+          onPress={limpiarOracion}
+          style={{
+            backgroundColor: "#0077B6",
+            width: 300,
+            height: 40,
+            borderRadius: 10,
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 20,
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontWeight: "bold",
+            }}
+          >
+            Orar De nuevo
+          </Text>
+        </TouchableOpacity>
+      </View>
 
             <View
-            style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: 40,
-                
-            }}
+            //style={styles.containerModal}
             >
-                    <Text
-                    style={{    
-                        fontSize: 20,
-                        fontWeight: '500',
-                        textAlign: 'center',
-                        margin: 20,
-                    }}
-                
-                    
-                    >
-                        Ora de manera personalizada para tus seres queridos y comparte  en tus redes sociales.
-                    </Text>
-            </View>
+            <Modal
+            //style={styles.modalInfo}
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+        >
            
-
-            <View
-            style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: 80,
-            }}
-            >
-                <TextInput
-
-                onChangeText={(nombre) => setNombre(nombre)}
-                value={nombre}
-                    style={{
-                        height: 'auto',
-                        width: 300,
-                        minHeight: 40,
-                        borderColor: 'gray',
-                        borderWidth: 1,
-                        borderRadius: 10,
-                        textAlign: 'center',
-                    }}
-                    placeholder="Nombre"
-                />
-
-                <TextInput
-                onChangeText={(intencion) => setIntencion(intencion)}
-                value={intencion}
-                    style={{
-                        height: 'auto',
-                        width: 300,
-                        minHeight: 40,
-                        borderColor: 'gray',
-                        borderWidth: 1,
-                        borderRadius: 10,
-                        textAlign: 'center',
-                        marginTop: 10,
-                    }}
-                    placeholder="Intención"
-
-                />
-
-                <TouchableOpacity
-                onPress={creatOrc}
-
-                style={{
-                    backgroundColor: '#841584',
-                    width: 300,
-                    height: 40,
-                    borderRadius: 10,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    marginTop: 20,
-                }}
-                >
-                    <Text
-                    style={{
-                        color: 'white',
-                        fontWeight: 'bold',
-                    }}
-                    >
-                        Crear Oración
-                    </Text>
-                </TouchableOpacity>
-
-
-
-                
-                <TouchableOpacity
-                onPress={limpiarOracion}
-                style={{
-                    backgroundColor: '#0077B6',
-                    width: 300,
-                    height: 40,
-                    borderRadius: 10,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    marginTop: 20,
-                }}
-                >
-                    <Text
-                    style={{
-                        color: 'white',
-                        fontWeight: 'bold',
-                    }}
-                    >
-                        Orar De nuevo
-                    </Text>
-                </TouchableOpacity>
-            
-            </View>
-
-            <View style={styles.containerModal} >
-                <Modal
-                    animationType="slide"
-                    transparent={true}
-                    visible={modalVisible}
-                    onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-            setModalVisible(!modalVisible);
-            }}>
-
-                    <View
-                    style={{
-                        flex: 1,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        backgroundColor: 'rgba(0,0,0,0.5)',
-                    }}
-                    >
-                        <View
-                        style={{
-                            backgroundColor: 'white',
-                            width: '80%',
-                            height: '40%',
-                            borderRadius: 20,
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}
-                        >
-                            <Text
-                            style={{
-                                fontSize: 20,
-                                fontWeight: 'bold',
-                                textAlign: 'center',
-                            }}
-                            >
-                                { txtModal }
-                            </Text>
-                        </View>
-                        </View>
-                </Modal>
-            </View>
-
+           <View style={styles.centeredView}>
+          <View style={styles.modalInfoText}>
+            <Text>{txtModal}</Text>
+            <Button title='Cerrar' onPress={cerrarModal} />
+          </View>
         </View>
-    );
-    
-}
+      
+            </Modal>
+            </View>
+    </View>
+  );
+};
 
-export default CrearOrc;    
-
+export default CrearOrc;
 
 const styles = StyleSheet.create({
-    containerModal: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: 22,
-
-    },
-   
+  // Estilo para centrar el contenido del modal
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.5)", // Fondo semitransparente
+  },
+  modalInfoText: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 45,
+    alignItems: "center",
+    // Ajustes para asegurar que el contenido interno también esté centrado
+    alignSelf: "center",
+    margin: 20,
+  },
 });

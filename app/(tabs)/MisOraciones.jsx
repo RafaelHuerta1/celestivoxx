e4f408ef14b45aba488b9807f74279f9ad6939ff
RafaelHuerta1 <010530713@alumno.utel.edu.mx>
2024-07-11 18:35:13 -0600

    Refactor MisOraciones component and add ScrollView for better user experience

--- app/(tabs)/MisOraciones.jsx
+++ app/(tabs)/MisOraciones.jsx
@@ -1,5 +1,5 @@
 import React , {useEffect, useState} from "react";
-import { View, Text, Image, TouchableOpacity, Button , StyleSheet} from "react-native";
+import { View, Text, Image, TouchableOpacity, Button , StyleSheet, ScrollView } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 
 
@@ -15,25 +15,38 @@ const MisOraciones = () => {
     useEffect(() => {
         console.log('Mis Oraciones useEffect');
         setMisOraciones([...misOraciones, {nombre: nombre, oracion: oracion, categorias: categorias}]);
-        console.log('Mis Oraciones useEffect', misOraciones);
+        console.log('Mis Oraciones useEffect', misOraciones.oracion);
     }, [oracion, nombre, categorias]);
 
-
+    // add un scrollview
     const createCard = (oracion, index) => {
         return (
-          <View
-            style={styles.containerMainCard}
-            key={index}
-          >
-            <Text style={styles.txtStyle}> {oracion.nombre} </Text>
-            <Text style={styles.txtParrafo1}> {oracion.categorias} </Text>
-          </View>
+
+            < > 
+
+                <View
+                    style={styles.containerMainCard}
+                    key={index}
+                >
+                           
+                            <Text style={styles.txtStyle}> NOMBRE: {oracion.nombre} </Text>
+                            <Text style={styles.txtParrafo1}> CATEGORIA: {oracion.categorias} </Text>
+
+                            <View>
+                                <Text style={styles.txtVerMas}> Ver Oracion Completa </Text>
+                            </View>
+                    
+                </View>
+          </>
+
         );
       };
       
 
 
         return (
+            <ScrollView>
+
             <View
              style={{
                 justifyContent : "center",
@@ -50,6 +63,7 @@ const MisOraciones = () => {
                 )}
 
             </View>
+            </ScrollView>
         );  
 
     
@@ -60,11 +74,30 @@ export default MisOraciones;
 
 const styles = StyleSheet.create({
     containerMainCard: {
-        backgroundColor: '#f0f0f0',
+        backgroundColor: 'white',
         width: '80%',
-        height: 200,
+        height: 120,
         borderRadius: 20,
         margin: 10,
-        padding: 10,
+        padding: 13,
+
+    },
+    txtStyle: {
+        color: 'black',
+        fontWeight: 'bold',
+        textAlign: 'start',
+        fontSize: 20,
+    },
+    txtParrafo1: {
+        color: 'black',
+        fontWeight: '400',
+        textAlign: 'start',
+        marginTop: 10,
+    },
+    txtVerMas: {
+        color: 'blue',
+        fontWeight: '500',
+        textAlign: 'start',
+        marginTop: 10,
     },
   });
